f7fb38762b88fac39fb012f77faaff7bed927f2d
HeloisyKarine <hkbss1@aluno.ifal.edu.br>
2022-09-10 01:20:09 +0000

    divisao_feita

--- api/src/main/java/app/App.java
+++ api/src/main/java/app/App.java
@@ -5,8 +5,7 @@ import io.jooby.Jooby;
 public class App extends Jooby {
 
   {
-
-    get("/", ctx -> "Welcome to Jooby!");
+    mvc(new Divisao());
   }
 
   public static void main(final String[] args) {

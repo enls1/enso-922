a3771098deabdb342cfaabfa5d546dba0f6f58b0
LiviaVerissimo <lvvl1@aluno.ifal.edu.br>
2022-09-12 20:01:28 +0000

    divisaoAlterada

--- api/src/main/java/app/Divisao.java
+++ api/src/main/java/app/Divisao.java
@@ -7,7 +7,6 @@ public class Divisao{
     @GET
     public double calcularDivisao(@PathParam double op1, @PathParam double op2){
         double resultado=op1/op2;
-        return resultado;
-           
+        return resultado;      
     }
 }

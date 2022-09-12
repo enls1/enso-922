abcef55fbc4bdc7b984193d3f34c25ac15f4a0c0
LiviaVerissimo <lvvl1@aluno.ifal.edu.br>
2022-09-12 19:42:46 +0000

    subtracaoAlterada

--- api/src/main/java/app/Subtracao.java
+++ api/src/main/java/app/Subtracao.java
@@ -3,8 +3,7 @@ package app;
 import io.jooby.annotations.*;
     
 @Path("/sub/{op1}/{op2}")
-public class Subtracao{
-   
+public class Subtracao{   
       @GET
       public double cacularSubtracao(@PathParam double op1 , @PathParam double op2){
              double resultado = op1-op2; 

5ff7874dac3dd70f33830a674c15768b335929af
alineze <aac11@aluno.ifal.edu.br>
2022-09-12 20:11:51 +0000

    mudança realizada

--- api/src/main/java/app/Soma.java
+++ api/src/main/java/app/Soma.java
@@ -4,7 +4,6 @@ import io.jooby.annotations.*;
 
 @Path("/soma/{op1}/{op2}")
 public class Soma{
-
     @GET
     public double calcularSoma(@PathParam double op1, @PathParam double op2){
         double resultado = op1+op2;

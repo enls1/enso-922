979e2d2ad7f58590c58a638ec66945b86bb197aa
LiviaVerissimo <lvvl1@aluno.ifal.edu.br>
2022-09-12 20:24:59 +0000

    multiplicaçao

--- api/src/main/java/app/Multiplicacao.java
+++ api/src/main/java/app/Multiplicacao.java
@@ -4,6 +4,8 @@ import io.jooby.annotations.*;
 
 @Path("/multi/{op1}/{op2}")
 public class Multiplicacao{
+
+    
     @GET
     public double calcularMultiplicacao(@PathParam double op1 , @PathParam double op2){
            double resultado = op1*op2;
